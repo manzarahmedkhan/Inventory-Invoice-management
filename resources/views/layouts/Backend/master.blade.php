7875c311aaa7313c6c6a2140258bbc0aff81f378
manzarahmedkhan <manzarahmedkhan@gmail.com>
2021-11-29 01:01:01 +0530

    changes

--- resources/views/layouts/Backend/master.blade.php
+++ resources/views/layouts/Backend/master.blade.php
@@ -39,7 +39,7 @@
       <footer class="sticky-footer bg-white">
         <div class="container my-auto">
           <div class="copyright text-center my-auto">
-            <span>Copyright &copy; ArifMehrab 2020</span>
+            <span>Copyright &copy; TechMonstre 2021</span>
           </div>
         </div>
       </footer>
