6cb72c07343ccbd1d0e52671b68162512008ae11
mak <manzar@etfmatic.com>
2021-12-04 02:11:18 +0530

    changes

--- app/Http/Controllers/Backend/supplierController.php
+++ app/Http/Controllers/Backend/supplierController.php
@@ -25,7 +25,7 @@ class supplierController extends Controller
         $validation = $request->validate([
         	'name'    => 'required',
         	// 'mobile'  => 'required',
-        	// 'email'   => 'required|email'
+        	'email'   => 'email'
         ]);
         // Insert Data
         $supplierInsert = new supplier;
@@ -48,8 +48,8 @@ class supplierController extends Controller
         // validation
         $validation = $request->validate([
             'name'    => 'required',
-            'mobile'  => 'required',
-            'email'   => 'required|email'
+            // 'mobile'  => 'required',
+            'email'   => 'email'
         ]);
         // Update
         $supplierUpdate = supplier::find($id);
