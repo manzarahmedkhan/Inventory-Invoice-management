c5abbbd544b281dba4388718e9d03ee4f14e1dfa
mak <manzar@etfmatic.com>
2022-01-01 01:46:20 +0530

    changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,7 +32,7 @@
             <!-- Nested Row within Card Body -->
             <div class="row">
               <div class="col-lg-6 d-none d-lg-block bg-login-image">
-                  <img class="mt-2" width="500" src="{{ asset('assets/Backend/images/arifmehrab.jpg') }}">
+                  <!-- <img class="mt-2" width="500" src="{{ asset('assets/Backend/images/arifmehrab.jpg') }}"> -->
               </div>
               <div class="col-lg-6">
                 <div class="p-5">
