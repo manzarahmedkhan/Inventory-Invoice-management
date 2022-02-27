9d071d23d25de8ec7a4769f87a93dc7baf1d9305
mak <manzar@datavoice.co.in>
2022-02-27 12:07:51 +0530

    changes in invoice report

--- resources/views/layouts/Backend/pdf/invoiceDaily.blade.php
+++ resources/views/layouts/Backend/pdf/invoiceDaily.blade.php
@@ -17,17 +17,24 @@
 		<tr>
 			<td width="14%">Date</td>
 			<td>Customer Name-Mobile</td>
-			<td width="11%">Invoice #</td>
-			<td width="14%">Amount</td>
+			<td width="10%">Invoice#</td>
+			<td width="13%">Amount</td>
 			<td width="11%">Vat(15%)</td>
-			<td width="14%">Total</td>
+			<td width="13%">Total</td>
 		</tr>
 	</thead>
 	<tbody>
 		@php 
 		$bankTotal =0;
 		$cashTotal =0;
-		$subTotal = 0;
+		$subAmount = 0;
+		$cashAmount = 0;
+		$bankAmount = 0;
+		$finalTotal = 0;
+		$cashVat = 0;
+		$bankVat = 0;
+		$finalVat = 0;
+		$finalAmount = 0;
 		@endphp
 		@foreach($invoices as $invoice)
 		<tr>
@@ -41,37 +48,46 @@
 			@php
 			$totalAmount = $invoice->payment->total_amount ?? $invoice->total_amount;
 			$vatAmount = $invoice->vat_amount ?? 0.00;
+			$subAmount = $totalAmount - $vatAmount;
 			@endphp
-			<td align="right">{{ $invoice->invoice_no }}</td>
-			<td align="right">{{ number_format($totalAmount - $vatAmount,2) }}</td>
+			<td>{{ $invoice->invoice_no }}</td>
+			<td align="right">{{ number_format($subAmount,2) }}</td>
 			<td align="right">{{ number_format($vatAmount,2) }}</td>
 			<td align="right">{{ number_format($totalAmount,2) }}</td>
 		</tr>
 		@php
 		$paymentMode = $invoice->payment_mode ?? null;
 		if($paymentMode == "Bank"){
+			$bankAmount += $subAmount;
+			$bankVat += $vatAmount;
 			$bankTotal += $totalAmount;
 		}else{
-			$cashTotal += $totalAmount;
+			$cashAmount += $subAmount;
+			$cashVat += $vatAmount;
+			$cashTotal  += $totalAmount;
 		}
-		$subTotal += $totalAmount;
+		$finalAmount += $subAmount;
+		$finalVat += $vatAmount;
+		$finalTotal += $totalAmount;
 		@endphp
 		@endforeach
 		<tr>
-			<td style="text-align: right; color:red;" colspan="5">Vat Total:-</td>
-			<td style="text-align: right; color:red;">{{ number_format($cashTotal,2) }}</td>
+			<td style="text-align: right; color:blue;" colspan="3">Cash Total:-</td>
+			<td style="text-align: right; color:blue;">{{ number_format($cashAmount,2) }}</td>
+			<td style="text-align: right; color:blue;">{{ number_format($cashVat,2) }}</td>
+			<td style="text-align: right; color:blue;">{{ number_format($cashTotal,2) }}</td>
 		</tr>
 		<tr>
-			<td style="text-align: right; color:red;" colspan="5">Cash Total:-</td>
-			<td style="text-align: right; color:red;">{{ number_format($cashTotal,2) }}</td>
-		</tr>
-		<tr>
-			<td style="text-align: right; color:red;" colspan="5">Bank Total:-</td>
+			<td style="text-align: right; color:red;" colspan="3">Bank Total:-</td>
+			<td style="text-align: right; color:red;">{{ number_format($bankAmount,2) }}</td>
+			<td style="text-align: right; color:red;">{{ number_format($bankVat,2) }}</td>
 			<td style="text-align: right; color:red;">{{ number_format($bankTotal,2) }}</td>
 		</tr>
 		<tr>
-			<td style="text-align: right; color:green;" colspan="5">Total:-</td>
-			<td style="text-align: right; color:green;">{{ number_format($subTotal,2) }}</td>
+			<td style="text-align: right; color:green;" colspan="3">Total:-</td>
+			<td style="text-align: right; color:green;">{{ number_format($finalTotal,2) }}</td>
+			<td style="text-align: right; color:green;">{{ number_format($finalTotal,2) }}</td>
+			<td style="text-align: right; color:green;">{{ number_format($finalTotal,2) }}</td>
 		</tr>
 	</tbody>
 </table>
